c2b0a8ff91b87ea51c891921c3b601ad31b906a1
Mr Scott <mscott@brightoncollege.net>
2019-01-29 14:35:32 +0000

    Got Validation working and connected to the database

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
 from flask import Flask, request, render_template
+import sqlite3
+from flask import g
 
 app = Flask(__name__)
 
 
+DATABASE = './users.db'
+
+
+def get_db():
+
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
 def valid_login(username, password):
-    return True
+    user = query_db('select * from User where username = ? and password = ?', [username, password], one=True)
+    if user is None:
+        return False
+    else:
+        return True
 
 
 def log_the_user_in(username):
